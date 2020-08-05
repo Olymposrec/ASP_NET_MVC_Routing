f0b862254765ccde760a83bd46098ed8126d6767
Melih <Olymposrec@users.noreply.github.com>
2020-08-05 16:58:15 +0300

    ASP_NET_MVC_Routing
    ASP.NET MVC' nin bu bölümünde Routing konusuna bakacağız. Kodlar klasörde mevcut.

---
+++ ASP_NET_MVC_Routing/App_Start/RouteConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ASP_NET_MVC_Routing
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+             name: "Makaleler",
+             url: "Makale/Goster/{makaleadi}",
+             defaults: new { controller = "Makale", action = "Goster",makaleadi = UrlParameter.Optional }
+         );
+            routes.MapRoute(
+              name: "About",
+              url: "About",
+              defaults: new { controller = "Home", action = "Index3", id = UrlParameter.Optional }
+          );
+            routes.MapRoute(
+               name: "Contact",
+               url: "Contact",
+               defaults: new { controller = "Home", action = "Index2", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+               name: "Homepage",
+               url: "Homepage",
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+           );
+
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Default2",
+                url: "{controller}/Belge/{belgeno}",
+                defaults: new { controller = "Home", action = "Belge", belgeno = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ASP_NET_MVC_Routing/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_NET_MVC_Routing.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+        public ActionResult Index3()
+        {
+            return View();
+        }
+        public ActionResult Show(int? id)
+        {
+            return View();
+        }
+    }
+    
+}
